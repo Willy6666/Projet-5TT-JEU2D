da69bc0f842a02b268b456d390e9eb054dc29db2
willy <robin2004bourguignon@gmail.com>
2021-11-25 14:23:30 +0100

    Animation camera + ...

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,8 +6,8 @@ public class CameraFollow : MonoBehaviour
     public float timeOffset;
     public Vector3 posOffset;
 
-    private Vector3 velocity;  
-    
+    private Vector3 velocity;
+
     void Update()
     {
         transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+
     public float moveSpeed;
     public float jumpForce;
 
@@ -13,7 +13,10 @@ public class PlayerMovement : MonoBehaviour
     public Transform GroundCheckRight;
 
     public Rigidbody2D rb;
-    private Vector3 velocity = Vector3.zero;    
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    
+    private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
@@ -21,24 +24,41 @@ public class PlayerMovement : MonoBehaviour
 
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
-    if (Input.GetButtonDown("Jump") && isGrounded)
-    {
-        isJumping = true;
-    }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
 
         MovePlayer(horizontalMovement);
+
+        Flip(rb.velocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", characterVelocity);
     }
 
-    
+
     void MovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
-        if(isJumping == true)
+        if (isJumping == true)
         {
             rb.AddForce(new Vector2(0f, jumpForce));
             isJumping = false;
+        }
+    }
+
+    void Flip(float _velocity)
+    {
+        if(_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false; 
+        }else if(_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+
         }
     }
 }
